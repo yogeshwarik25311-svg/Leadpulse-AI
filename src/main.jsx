e9757191e6d2de90ea0044e776ad8bc607be40a7
yogeshwarik25311-svg <yogeshwarik25311@gmail.com>
2026-07-28 22:01:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("#root not found in index.html");
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
